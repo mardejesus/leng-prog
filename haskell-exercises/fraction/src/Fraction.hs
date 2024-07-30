7a995fbf4fbedb0a5fac74425b4482ccfccb93eb
mardejesus <mardejesus001@gmail.com>
2024-07-29 23:36:10 -0300

    tp fraction pasando

--- haskell-exercises/fraction/src/Fraction.hs
+++ haskell-exercises/fraction/src/Fraction.hs
@@ -5,27 +5,43 @@ type Fraction = (Int, Int)
 -- Implement the `add` Function
 
 add :: Fraction -> Fraction -> Fraction
-add n d = error "Implement it"
+add (a, b) (c, d) = (numerador, denominador)
+    where
+        numerador = (a*d)+(b*c)
+        denominador = b*d
 
 -- Implement the `sub` Function
 
 sub :: Fraction -> Fraction -> Fraction
-sub n d = error "Implement it"
+sub (a, b) (c, d) = (numerador, denominador)
+    where
+        numerador = (a*d)-(b*c)
+        denominador = b*d
 
 -- Implement the `mul` Function
 
 mul :: Fraction -> Fraction -> Fraction
-mul n d = error "Implement it"
+mul (a,b) (c,d) = (numerador, denominador)
+    where
+        numerador = a*c
+        denominador = b*d
 
 -- Implement the `divide` Function
 
 divide :: Fraction -> Fraction -> Fraction
-divide n d = error "Implement it"
+divide (a,b) (c,d) = (numerador, denominador)
+    where
+        numerador = a*d
+        denominador = b*c
 
 -- Implement the `hcf` Function
 
 hcf :: Int -> Int -> Int
-hcf n d = error "Implement it"
+hcf n d = hcfRecursivo n d n
 
-
-    
+-- busca de forma recursiva de mayor a menor, un número que sea divisor de ambos parámetros
+hcfRecursivo :: Int -> Int -> Int -> Int
+hcfRecursivo a b divisor
+    | divisor==1 = 1
+    | (mod a divisor == 0) && (mod b divisor == 0) = divisor
+    | otherwise = hcfRecursivo a b (divisor-1)
