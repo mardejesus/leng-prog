6f1e50be57e3ebc41eacec915cbc3b4c8e680c9b
mardejesus <mardejesus001@gmail.com>
2024-10-13 16:39:12 -0300

    queen attack

--- rust-exercises/queen-attack/src/lib.rs
+++ rust-exercises/queen-attack/src/lib.rs
@@ -8,16 +8,22 @@ pub struct Queen {
 
 impl ChessPosition {
     pub fn new(rank: i32, file: i32) -> Option<Self> {
-        panic!("Implement me");
+        return if rank >= 0 && rank < 8 && file >= 0 && file < 8 { // chequea si está dentro de los límites del tablero
+            Some(ChessPosition(rank, file))
+        } else {
+            None
+        }
     }
 }
 
 impl Queen {
     pub fn new(position: ChessPosition) -> Self {
-        panic!("Implement me");
+        return Queen {position};
     }
 
     pub fn can_attack(&self, other: &Queen) -> bool {
-    panic!("Implement me");
+        return (self.position.0-other.position.0).abs() == (self.position.1-other.position.1).abs() ||
+            self.position.0 == other.position.0 ||
+            self.position.1 == other.position.1;
 }
 }
