5e7594cc190d09707a44935d68df8c7d6940cb0d
mardejesus <mardejesus001@gmail.com>
2024-10-13 16:08:22 -0300

    health statistics

--- rust-exercises/health-statistics/src/lib.rs
+++ rust-exercises/health-statistics/src/lib.rs
@@ -5,15 +5,15 @@ pub struct User {
 }
 
 impl User {
-    pub fn new(name: &str, age: u32, weight: f32) -> Self { panic!("Implement me!") }
+    pub fn new(name: &str, age: u32, weight: f32) -> Self { return User{name:(*name).to_string(), age, weight} }
 
-    pub fn name(&self) -> &str { panic!("Implement me!") }
+    pub fn name(&self) -> &str { return &self.name }
 
-    pub fn age(&self) -> u32 { panic!("Implement me!") }
+    pub fn age(&self) -> u32 { return self.age }
 
-    pub fn weight(&self) -> f32 { panic!("Implement me!") }
+    pub fn weight(&self) -> f32 { return self.weight }
 
-    pub fn set_age(&mut self, new_age: u32) { panic!("Implement me!") }
+    pub fn set_age(&mut self, new_age: u32) { self.age = new_age; }
 
-    pub fn set_weight(&mut self, new_weight: f32) { panic!("Implement me!") }
+    pub fn set_weight(&mut self, new_weight: f32) { self.weight = new_weight; }
 }
